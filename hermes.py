76e2cf06787eace6a0b3d25a1507c32e29642912
StealthyPanda <shaikm259@gmail.com>
2026-04-19 02:22:51 +0400

    Added config arg for build command
    Now you can use multiple config files for multiple builds (say dev and build). Choose config in the build command. Defaults to `hermes.json`.

--- hermes.py
+++ hermes.py
@@ -1,134 +1,138 @@
-
+import json
+import os
+import shutil
+import time
+import traceback
 from typing import Annotated
 
-import typer, os, rich, json, shutil, traceback, time
-
-from lib import *
-from rich.prompt import Prompt, Confirm 
-from rich.panel import Panel
+import rich
+import typer
 from rich.console import Console
+from rich.panel import Panel
+from rich.prompt import Confirm, Prompt
 from rich.traceback import Traceback
 
+from lib import *
 
-app = typer.Typer(help='A simple no nonsense build system for C/C++')
+app = typer.Typer(help="A simple no nonsense build system for C/C++")
 
-hermes_file_path = os.path.join(os.path.curdir, 'hermes.json')
-hermes_dir_path  = os.path.join(os.path.curdir, '.hermes')
-
+hermes_file_path = os.path.join(os.path.curdir, "hermes.json")
+hermes_dir_path = os.path.join(os.path.curdir, ".hermes")
 
 
 @app.command()
 def init():
     """Initialises a hermes build module in current folder"""
     if os.path.exists(hermes_file_path):
-        res = Confirm.ask(
-            "Hermes config file already exists; reset?",
-            default=False
-        )
+        res = Confirm.ask("Hermes config file already exists; reset?", default=False)
         if res:
-            with open(hermes_file_path, 'w') as file:
+            with open(hermes_file_path, "w") as file:
                 file.write(json.dumps(template, indent=4))
     else:
-        with open(hermes_file_path, 'w') as file:
+        with open(hermes_file_path, "w") as file:
             file.write(json.dumps(template, indent=4))
-    
+
     if os.path.exists(hermes_dir_path):
         res = Confirm.ask("Hermes data already exists; reset?", default=False)
         if res:
-            try : shutil.rmtree(hermes_dir_path)
+            try:
+                shutil.rmtree(hermes_dir_path)
             except OSError as e:
-                Panel.fit(f'{e.filename}:\n{e.strerror}')
+                Panel.fit(f"{e.filename}:\n{e.strerror}")
                 typer.Exit(-1)
     else:
         os.mkdir(hermes_dir_path)
-        
-    gif = os.path.join(os.path.curdir, '.gitignore')
+
+    gif = os.path.join(os.path.curdir, ".gitignore")
     if os.path.exists(gif):
-        with open(gif, 'a') as file:
-            file.write('\n\n')
-            file.write('#----- hermes -----\n')
-            file.write('.hermes/\n')
-            file.write('#------------------\n')
-            file.write('\n\n')
-    
+        with open(gif, "a") as file:
+            file.write("\n\n")
+            file.write("#----- hermes -----\n")
+            file.write(".hermes/\n")
+            file.write("#------------------\n")
+            file.write("\n\n")
 
 
 @app.command()
 def build(
-        debug : Annotated[bool, typer.Option(
-            help='Outputs debug info'
-        )] = False,
-        verbose : Annotated[bool, typer.Option(
-            help='Outputs debug info + commands and everything'
-        )] = False,
-        force : Annotated[bool, typer.Option(
-            help='Forces recompilation of all files, ignoring tracks'
-        )] = False,
-    ):
+    config: Annotated[
+        str, typer.Argument(help=".json config file to use for build info")
+    ] = "hermes",
+    debug: Annotated[bool, typer.Option(help="Outputs debug info")] = False,
+    verbose: Annotated[
+        bool, typer.Option(help="Outputs debug info + commands and everything")
+    ] = False,
+    force: Annotated[
+        bool, typer.Option(help="Forces recompilation of all files, ignoring tracks")
+    ] = False,
+):
     """Builds the current hermes module"""
+    global hermes_file_path
+    hermes_file_path = os.path.join(os.path.curdir, f"{config}.json")
     if not os.path.exists(hermes_file_path):
         raise FileNotFoundError(
-            "Not a hermes module! (Use `hermes init` to make one here)"
+            "Not a hermes module / invalid config file! (Use `hermes init` to make one here)"
         )
-    with open(hermes_file_path, 'r') as file:
+    with open(hermes_file_path, "r") as file:
         bm = BuildModule(
             root=os.path.abspath(os.path.curdir),
             config=json.loads(file.read()),
-            debug=debug or verbose, verbose=verbose,
-            force=force
+            debug=debug or verbose,
+            verbose=verbose,
+            force=force,
         )
-    
+
     start = time.time()
     build_module(bm)
     end = time.time()
     rich.print(f"[dim]Finished building in[/] [yellow]{end - start:.2f}s[/]")
-    
-    
-    if (bm.config['target']['type'] == 'exe') and bm.config['target']['run']:
+
+    if (bm.config["target"]["type"] == "exe") and bm.config["target"]["run"]:
         print()
         start = time.time()
         try:
-            code = os.system(os.path.abspath(bm.config['target']['exeout']))
+            code = os.system(os.path.abspath(bm.config["target"]["exeout"]))
         except KeyboardInterrupt:
             pass
         end = time.time()
 
-        clr = 'red' if code else 'green'
+        clr = "red" if code else "green"
         rprint(
             f"\n[dim]Finished execution with[/] [{clr}]exit code {code}[/] "
-            f"[dim]in[/] [yellow]{end-start:.2f}s[/]"
+            f"[dim]in[/] [yellow]{end - start:.2f}s[/]"
         )
 
 
 @app.command()
 def gen(
-        filetype : Annotated[str, typer.Argument(
-            help='Type of file'
-        )] = 'clangd-compile-flags'
-    ):
+    filetype: Annotated[
+        str, typer.Argument(help="Type of file")
+    ] = "clangd-compile-flags",
+):
     """Generates files for LSPs and stuff."""
     if not os.path.exists(hermes_file_path):
         raise FileNotFoundError(
             "Not a hermes module! (Use `hermes init` to make one here)"
         )
-    with open(hermes_file_path, 'r') as file:
+    with open(hermes_file_path, "r") as file:
         bm = BuildModule(
             root=os.path.abspath(os.path.curdir),
             config=json.loads(file.read()),
-            debug=False, verbose=False,
-            force=False
+            debug=False,
+            verbose=False,
+            force=False,
         )
-    if filetype == 'clangd-compile-flags':
-        fp = os.path.join(bm.root, 'compile_flags.txt')
-        with open(fp, 'w') as file:
+    if filetype == "clangd-compile-flags":
+        fp = os.path.join(bm.root, "compile_flags.txt")
+        with open(fp, "w") as file:
             file.write(
-                '\n'.join(
-                    [f'-I{x}'   for x in bm.config["libincdirs"]] +
-                    [f'-l{x}'     for x in bm.config["libs"]] + 
-                    [f'-L{x}'   for x in bm.config["libdirs"]]
-                ) + '\n'
+                "\n".join(
+                    [f"-I{x}" for x in bm.config["libincdirs"]]
+                    + [f"-l{x}" for x in bm.config["libs"]]
+                    + [f"-L{x}" for x in bm.config["libdirs"]]
+                )
+                + "\n"
             )
-        
 
 
 @app.command()
@@ -138,15 +142,16 @@ def run():
         raise FileNotFoundError(
             "Not a hermes module! (Use `hermes init` to make one here)"
         )
-    with open(hermes_file_path, 'r') as file:
+    with open(hermes_file_path, "r") as file:
         bm = BuildModule(
             root=os.path.abspath(os.path.curdir),
             config=json.loads(file.read()),
-            debug=False, verbose=False,
-            force=False
+            debug=False,
+            verbose=False,
+            force=False,
         )
-    
-    eo = bm.config['target']['exeout']
+
+    eo = bm.config["target"]["exeout"]
     eo = os.path.abspath(eo)
     if not os.path.exists(eo):
         rprint(
@@ -154,7 +159,7 @@ def run():
             "set target type to `exe` and run `hermes build`[/]"
         )
         return
-    
+
     start = time.time()
     try:
         code = os.system(eo)
@@ -162,39 +167,43 @@ def run():
         pass
     end = time.time()
 
-    clr = 'red' if code else 'green'
+    clr = "red" if code else "green"
     rprint(
         f"\n[dim]Finished execution with[/] [{clr}]exit code {code}[/] "
-        f"[dim]in[/] [yellow]{end-start:.2f}s[/]"
+        f"[dim]in[/] [yellow]{end - start:.2f}s[/]"
     )
 
 
-
 console = Console()
+
+
 def main():
-    try: app()
+    try:
+        app()
     except Exception as e:
         exc_type, exc_value, traceback = sys.exc_info()
-        
+
         while traceback.tb_next:
             traceback = traceback.tb_next
-        
+
         sl = False
-        if hasattr(e, 'bm'):
+        if hasattr(e, "bm"):
             sl = e.bm.verbose
-        
+
         rich_tb = Traceback.from_exception(
-            exc_type, exc_value, traceback,
-            show_locals= sl ,
+            exc_type,
+            exc_value,
+            traceback,
+            show_locals=sl,
             # theme="monokai",
             theme="inkpot",
-            extra_lines=3
+            extra_lines=3,
         )
-        
-        if hasattr(e, 'debug'):
+
+        if hasattr(e, "debug"):
             if e.debug:
                 console.print(rich_tb)
         else:
             console.print(rich_tb)
-        
+
         typer.Exit(-1)
